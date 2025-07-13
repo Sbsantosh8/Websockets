68586e54553dd94bb80569a29c3ff0b946a5298a
Sbsantosh8 <santoshmudhiraj81@gmail.com>
2025-07-13 20:06:40 +0530

    worked on synchronous and asynchronous connection

--- Websockets/app/consumers.py
+++ Websockets/app/consumers.py
@@ -12,5 +12,23 @@ class MyWebsocketConsumer(WebsocketConsumer):
     def receive(self, text_data=None, bytes_data=None):
         print("Message Recieved from client...", text_data)
 
+        self.send(text_data=f"Server says: {text_data.upper()}")
+
     def disconnect(self, close_code):
         print("Websocket Disconnected...", close_code)
+
+
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+
+
+class AsyncEchoConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+        await self.send(text_data="WebSocket connected asynchronously!")
+
+    async def receive(self, text_data):
+        await self.send(text_data=f"You said (async): {text_data}")
+
+    async def disconnect(self, close_code):
+        print(f"WebSocket disconnected with code: {close_code}")
--- Websockets/app/routing.py
+++ Websockets/app/routing.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import consumers
 
 
-websocket_urlpatterns = [path("ws/wsc/", consumers.MyWebsocketConsumer.as_asgi())]
+websocket_urlpatterns = [
+    path("ws/wsc/", consumers.MyWebsocketConsumer.as_asgi()),
+    path("async/", consumers.AsyncEchoConsumer.as_asgi()),
+]
