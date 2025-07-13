bfee53effa675f4a928fcdfd18eeeeb333925820
Sbsantosh8 <santoshmudhiraj81@gmail.com>
2025-07-13 21:08:01 +0530

    worked on broadcasting message using redis

--- Websockets/app/consumers.py
+++ Websockets/app/consumers.py
@@ -32,3 +32,41 @@ class AsyncEchoConsumer(AsyncWebsocketConsumer):
 
     async def disconnect(self, close_code):
         print(f"WebSocket disconnected with code: {close_code}")
+
+
+
+
+
+
+
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+import json
+
+
+class AsyncChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_group_name = "global_chat"  # Fixed room name for simplicity
+
+        # Join group
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+
+        await self.accept()
+        await self.send(text_data="🟢 Connected to group chat.")
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data.get("message", "")
+
+        # Send message to group
+        await self.channel_layer.group_send(
+            self.room_group_name, {"type": "chat_message", "message": message}
+        )
+
+    # Receive message from group and send to WebSocket
+    async def chat_message(self, event):
+        message = event["message"]
+        await self.send(text_data=f"Broadcast: {message}")
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
--- Websockets/app/routing.py
+++ Websockets/app/routing.py
@@ -5,4 +5,5 @@ from . import consumers
 websocket_urlpatterns = [
     path("ws/wsc/", consumers.MyWebsocketConsumer.as_asgi()),
     path("async/", consumers.AsyncEchoConsumer.as_asgi()),
+    path("ws/chat/", consumers.AsyncChatConsumer.as_asgi()),
 ]
